8698cbffc419f1a01531cae6531a61e7ce26bb39
Brett Gordon <gordonbrett89@gmail.com>
2016-09-19 14:27:18 -0400

    in class changes

--- src/main/java/com/tiy/GameJsonRestController.java
+++ src/main/java/com/tiy/GameJsonRestController.java
@@ -1,11 +1,14 @@
 package com.tiy;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Brett on 9/19/16.
@@ -35,4 +38,30 @@ public class GameJsonRestController {
 
 		return gameList;
 	}
+
+	@RequestMapping(path = "/toggleGame.json", method = RequestMethod.GET)
+	public List<Game> toggleGame(int gameID) {
+		System.out.println("toggling game with ID " + gameID);
+		Game game = games.findOne(gameID);
+		game.name = "**" + game.name;
+		games.save(game);
+
+		return gamesJson();
+	}
+
+	@RequestMapping(path = "/addGame.json", method = RequestMethod.POST)
+	public List<Game> addGame(HttpSession session, @RequestBody Game game) throws Exception {
+		User user = (User)session.getAttribute("user");
+
+		if (user == null) {
+			throw new Exception("Unable to add game without an active user in the session");
+		}
+		game.user = user;
+
+		games.save(game);
+
+		return gamesJson();
+	}
+
+
 }
--- src/main/resources/static/js/tiy-ng-controller.js
+++ src/main/resources/static/js/tiy-ng-controller.js
@@ -21,6 +21,41 @@ angular.module('TIYAngularApp', [])
            console.log("Done with the promise, waiting for the callback")
        };
 
+        $scope.toggleGame = function(gameID) {
+            console.log("About to toggle game with ID " + gameID);
+
+            $http.get("/toggleGame.json?gameID=" + gameID)
+                .then(
+                    function success(response) {
+                        console.log(response.data);
+                        console.log("Game toggled");
+
+                        $scope.games = {};
+                        alert("About to refresh the games on the scope");
+
+                        $scope.games = response.data;
+                    },
+                    function error(response) {
+                        console.log("Unable to toggle game");
+                    });
+        };
+
+            $scope.addGame = function() {
+                console.log("About to add the following game " + JSON.stringify($scope.newGame));
+
+                $http.post("/addGame.json", $scope.newGame)
+                    .then(
+                        function successCallback(response) {
+                            console.log(response.data);
+                            console.log("Adding data to scope");
+                            $scope.games = response.data;
+                        },
+                        function errorCallback(response) {
+                            console.log("Unable to get data");
+                        });
+            };
+
+
 
         $scope.newGame = {};
     });
