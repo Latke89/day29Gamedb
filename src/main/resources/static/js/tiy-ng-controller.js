3d140cc7e7773ffb7949553aaf38c324abb9fb3e
Brett Gordon <gordonbrett89@gmail.com>
2016-09-28 11:31:34 -0400

    text fixes

--- src/main/resources/static/js/tiy-ng-controller.js
+++ src/main/resources/static/js/tiy-ng-controller.js
@@ -8,7 +8,7 @@ angular.module('TIYAngularApp', [])
            console.log("About to go get me some data!");
            $scope.name = "Javascript Master Guru";
 
-           $http.get("http://localhost:8080/games.json")
+           $http.get("/games.json")
                .then(
                    function successCallback(response) {
                        console.log(response.data);
